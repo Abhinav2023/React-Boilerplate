9687a8bc39610b9ce3725276846c2adf43e0f407
Abhinav2023 <abhinavbansal231101@gmail.com>
2019-12-25 10:44:55 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component,useState } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+const app=(props)=>{
+  const [personState,personStateHandler]=useState({
+    person:[
+      {name: "Abhinav", age:18},
+      {name: "Bansal", age:19} 
+    ]
+  });
+
+  const SwitchStateHandler=()=>{
+    return (
+      personStateHandler({
+        person:[
+          {name: "Ayushi", age:18},
+          {name: "Bansal", age:19} 
+        ]
+      })
+    )
+  }
+  return (
+    <div className="App">
+      <p>Hello, This is working</p>
+      <button onClick={SwitchStateHandler   }>Click Me!</button>
+      <Person name={personState.person[0].name} age={personState.person[0].age}/>
+      <Person name={personState.person[1].name} age={personState.person[1].age} >Hello, from chideren</Person>
+    </div>
+  );
+}
+
+export default app;
